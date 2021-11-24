c69a4c1e287a61c04181c20a96a6f42e12ded986
techHim <faithsacredoo3@gmail.com>
2021-11-24 22:47:43 +0800

    first-commit

---
+++ app.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', reveal);
+
+// var reveal = () => {
+//     var reveals = document.querySelectorAll('.reveal');
+
+//     for(var i = 0; i < reveals.length; i++) {
+//         var windowHeight = window.innerHeight;
+//         var revealTop = reveals[i].getBoundingClientRect().top;
+//         var revealPoint = 50;
+
+//         if(revealTop < windowHeight - revealPoint) {
+//             reveals[i].classList.add("active")
+//         }
+//     }
+// }
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+        for(var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var revealTop = reveals[i].getBoundingClientRect().top
+            var revealPoint = 50;
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active');
+            }
+        }
+}
