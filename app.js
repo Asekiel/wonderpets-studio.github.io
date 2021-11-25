0ed577a3faf79a78dbd26123fa6b45a195407615
techHim <faithsacredoo3@gmail.com>
2021-11-25 21:59:20 +0800

    added changes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+// ---------------------------
+// REVEAL 
+// -----------------------------
+
 window.addEventListener('scroll', reveal);
 
 // var reveal = () => {
@@ -27,3 +32,32 @@ function reveal() {
             }
         }
 }
+
+// ---------------------------
+// TEXT TYPING 
+// -----------------------------
+
+const texts = ['Photography', 'Modern Society', 'Passionate'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+// INVOKE THE FUNCTION WITHOUT CALLING
+
+(function type() {
+    if( count == texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0,++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 150);
+
+}());
