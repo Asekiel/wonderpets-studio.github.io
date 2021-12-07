d4656047186b0c2cfdaff1077482e8dd1d8f4ff0
techHim <faithsacredoo3@gmail.com>
2021-12-07 20:30:14 +0800

    changes

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 
+// fixed nav-bar
+window.addEventListener('scroll', () => {
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+
 // ---------------------------
 // REVEAL 
 // -----------------------------
 
 window.addEventListener('scroll', reveal);
-
-// var reveal = () => {
-//     var reveals = document.querySelectorAll('.reveal');
-
-//     for(var i = 0; i < reveals.length; i++) {
-//         var windowHeight = window.innerHeight;
-//         var revealTop = reveals[i].getBoundingClientRect().top;
-//         var revealPoint = 50;
-
-//         if(revealTop < windowHeight - revealPoint) {
-//             reveals[i].classList.add("active")
-//         }
-//     }
-// }
 
 function reveal() {
     var reveals = document.querySelectorAll('.reveal');
